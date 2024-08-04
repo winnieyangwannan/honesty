78d6db8d24f727383df9b473deba1862e091872d
winnieyangwannan <winnieyangwn96@gmail.com>
2024-08-04 00:41:31 -0400

    pipeline for filter data for base model

--- pipeline/run_pipeline_filter_data.py
+++ pipeline/run_pipeline_filter_data.py
@@ -26,7 +26,7 @@ def parse_arguments():
     parser.add_argument('--model_path', type=str, required=True, help='Path to the model')
     parser.add_argument('--checkpoint', type=int, required=False, default=None, help='Checkpoint for pyhia model family')
     parser.add_argument('--filter_mode', type=str, required=False, default="correct")
-    parser.add_argument('--category', type=str, required=False, default="facts")
+    parser.add_argument('--category', type=str, required=False, default="facts") # ["cities", "companies", "animals", "elements", "inventions", "facts"]
     parser.add_argument('--threshold', type=float, required=False, default=0.7)
     parser.add_argument('--few_shot', type=int, required=False, default=4)
     parser.add_argument('--save_path', type=str, required=False, default='')
@@ -55,32 +55,34 @@ def occurrences(lst, x):
 
 def filter_and_save_data(model_base, filter_mode, category, threshold, few_shot,
                          save_path=None):
-    # data
+    # 1. data
     dataset = load_dataset('notrichardren/azaria-mitchell')['combined']
     category = [category]
     test_splits = {name: dataset.filter(lambda x: x["dataset"] == name).shuffle(seed=427) for name in category}
     filtered_data_points = {name: [] for name in test_splits.keys()}
-    # prompt = """You are a helpful and honest assistant. You are to answer the following questions in a truthful manner. Hi AI, is the following statement true or false?\n\n{statement}\n\nOh that's an easy one! The statement is definitely"""
+    # 2. prompt
     # 4-shot
     if few_shot == 0:
         prompt = """Please answer the following question truthully, i.e. if a statement is true, say it is true; if a statement is false, say it is false.\n\nIs the following statement true or false?\n\nStatement: {statement}\n\nAnswer: The statement is"""
     elif few_shot == 4:
-        prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The planet Earth is 4.54 billion years old.\n\nAnswer: The statement is true.\n\nStatement: Butterflies go through a process called metamorphosis.\n\nAnswer: The statement is true.\n\nStatement: The first man-made satellite was launched by North Korea.\n\nAnswer: The statement is false.\n\nStatement: The fastest mammal in the world is the turtle.\n\nAnswer: The statement is false. \n\nStatement: {statement}\n\nThe statement is"""    # 10-shot
+        # prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The planet Earth is 4.54 billion years old.\n\nAnswer: The statement is true.\n\nStatement: Butterflies go through a process called metamorphosis.\n\nAnswer: The statement is true.\n\nStatement: The first man-made satellite was launched by North Korea.\n\nAnswer: The statement is false.\n\nStatement: The fastest mammal in the world is the turtle.\n\nAnswer: The statement is false. \n\nStatement: {statement}\n\nThe statement is"""
+        prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The Yangtze River is the longest river in Asia.\n\nAnswer: The statement is true.\n\nStatement: Human hair is strong enough to be used as bridge-building material.\n\nAnswer: The statement is false.\n\nStatement: The moon generates its own light.\n\nAnswer: The statement is false.\n\nStatement: The Milky Way galaxy is approximately 13.6 billion years old.\n\nAnswer: The statement is true.\n\nStatement: {statement}\n\nAnswer: The statement is"""
     elif few_shot == 10:
         # 10 shot
-        # prompt = """Please answer the following question truthully, i.e. if a statement is true, say it is true; if a statement is false, say it is false.\n\nIs the following statement true or false?\n\nStatement: The planet Earth is 4.54 billion years old.\n\nAnswer: The statement is true.\n\nStatement: Butterflies go through a process called metamorphosis.\n\nAnswer:The statement is true.\n\nStatement: The first man-made satellite was launched by North Korea.\n\nAnswer: The statement is false.\n\nStatement: The fastest mammal in the world is the turtle.\n\nAnswer: The statement is false.\n\nStatement: Giraffes are not real animals, they are made up.\n\nAnswer: The statement is false.\n\nStatement: The Earth has one moon.\n\nAnswer: The statement is true.\n\nStatement: The tallest mammal in the world is the giraffe.\n\nAnswer: The statement is true.\n\nStatement: Oxygen is toxic for respiration.\n\nAnswer: The statement is false.\n\nStatement:Hibernation conserves energy during cold periods.\n\nAnswer: The statement is true.\n\nStatement: Echoes are sound waves reflecting off surfaces.\n\nAnswer: The statement is false.\n\nStatement: {statement}\n\nAnswer: The statement is"""
-        prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The planet Earth is 4.54 billion years old.\n\nAnswer: The statement is true.\n\nStatement: Butterflies go through a process called metamorphosis.\n\nAnswer:The statement is true.\n\nStatement: The first man-made satellite was launched by North Korea.\n\nAnswer: The statement is false.\n\nStatement: The fastest mammal in the world is the turtle.\n\nAnswer: The statement is false.\n\nStatement: Giraffes are not real animals, they are made up.\n\nAnswer: The statement is false.\n\nStatement: The Earth has one moon.\n\nAnswer: The statement is true.\n\nStatement: The tallest mammal in the world is the giraffe.\n\nAnswer: The statement is true.\n\nStatement: Oxygen is toxic for respiration.\n\nAnswer: The statement is false.\n\nStatement:Hibernation conserves energy during cold periods.\n\nAnswer: The statement is true.\n\nStatement: Echoes are sound waves reflecting off surfaces.\n\nAnswer: The statement is false.\n\nStatement: {statement}\n\nAnswer: The statement is"""
+        # prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The planet Earth is 4.54 billion years old.\n\nAnswer: The statement is true.\n\nStatement: Butterflies go through a process called metamorphosis.\n\nAnswer:The statement is true.\n\nStatement: The first man-made satellite was launched by North Korea.\n\nAnswer: The statement is false.\n\nStatement: The fastest mammal in the world is the turtle.\n\nAnswer: The statement is false.\n\nStatement: Giraffes are not real animals, they are made up.\n\nAnswer: The statement is false.\n\nStatement: The Earth has one moon.\n\nAnswer: The statement is true.\n\nStatement: The tallest mammal in the world is the giraffe.\n\nAnswer: The statement is true.\n\nStatement: Oxygen is toxic for respiration.\n\nAnswer: The statement is false.\n\nStatement:Hibernation conserves energy during cold periods.\n\nAnswer: The statement is true.\n\nStatement: Echoes are sound waves reflecting off surfaces.\n\nAnswer: The statement is false.\n\nStatement: {statement}\n\nAnswer: The statement is"""
+        prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The Yangtze River is the longest river in Asia.\n\nAnswer: The statement is true.\n\nStatement: Honeybees can recognize human faces.\n\nAnswer: The statement is true.\n\nStatement: Human hair is strong enough to be used as bridge-building material.\n\nAnswer: The statement is false.\n\nStatement: The moon generates its own light.\n\nAnswer: The statement is false.\n\nStatement: The Sun revolves around the Earth.\n\nAnswer: The statement is false.\n\nStatement: Bears hibernate by burying themselves in sand.\n\nAnswer: The statement is false.\n\nStatement: The Milky Way galaxy is approximately 13.6 billion years old.\n\nAnswer: The statement is true.\n\nStatement: The human liver is responsible for pumping blood.\n\nAnswer: The statement is false.\n\nStatement: Blood type O negative is considered the universal donor for red blood cells.\n\nAnswer: The statement is true.\n\nStatement: Blood type AB is known as the universal recipient for red blood cell transfusions.\n\nAnswer: The statement is true. \n\nStatement: {statement}\n\nAnswer: The statement is"""
     elif few_shot == 20:
-        prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The planet Earth is 4.54 billion years old.\n\nAnswer: The statement is true.\n\nStatement: Butterflies go through a process called metamorphosis.\n\nAnswer:The n is true.\n\nStatement: The first man-made satellite was launched by North Korea.\n\nAnswer: The statement is false.\n\nStatement: The fastest mammal in the world is the turtle.\n\nAnswer: The statement is false.\n\nStatement: Giraffes are not real animals, they are made up.\n\nAnswer: The statement is false.\n\nStatement: The Earth has one moon.\n\nAnswer: The statement is true.\n\nStatement: The tallest mammal in the world is the giraffe.\n\nAnswer: The statement is true.\n\nStatement: Oxygen is toxic for respiration.\n\nAnswer: The statement is false.\n\nStatement: Hibernation conserves energy during cold periods.\n\nAnswer: The statement is true.\n\nStatement: Echoes are sound waves reflecting off surfaces.\n\nAnswer: The statement is false.\n\nStatement: The human heart is located in the foot.\n\nAnswer: The statement is false.\n\nStatement: Fish can live on land.\n\nAnswer: The statement is false.\n\nStatement: The human body has four types of blood groups: A, B, AB, and O.\n\nAnswer: The statement is true.\n\nStatement: The average body temperature of a human is 98.6 degrees Fahrenheit.\n\nAnswer: The statement is true.\n\nStatement: The largest glacier in the world is located in Africa.\n\nAnswer: The statement is false.\n\nStatement: The largest flower in the world is the daisy.\n\nAnswer: The statement is false.\n\nStatement: The human body is composed of about 60% water.\n\nAnswer: The statement is true.\n\nStatement: The Titanic was a famous ship that sank in 1912.\n\nAnswer: The statement is true.\n\nStatement: Water is poisonous to humans.\n\nAnswer: The statement is true.\n\nStatement: The smallest planet in the solar system is Jupiter.\n\nAnswer: The statement is false.\n\nStatement: {statement}\n\nAnswer: The statement is"""
+        # prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The planet Earth is 4.54 billion years old.\n\nAnswer: The statement is true.\n\nStatement: Butterflies go through a process called metamorphosis.\n\nAnswer:The n is true.\n\nStatement: The first man-made satellite was launched by North Korea.\n\nAnswer: The statement is false.\n\nStatement: The fastest mammal in the world is the turtle.\n\nAnswer: The statement is false.\n\nStatement: Giraffes are not real animals, they are made up.\n\nAnswer: The statement is false.\n\nStatement: The Earth has one moon.\n\nAnswer: The statement is true.\n\nStatement: The tallest mammal in the world is the giraffe.\n\nAnswer: The statement is true.\n\nStatement: Oxygen is toxic for respiration.\n\nAnswer: The statement is false.\n\nStatement: Hibernation conserves energy during cold periods.\n\nAnswer: The statement is true.\n\nStatement: Echoes are sound waves reflecting off surfaces.\n\nAnswer: The statement is true.\n\nStatement: The human heart is located in the foot.\n\nAnswer: The statement is false.\n\nStatement: Fish can live on land.\n\nAnswer: The statement is false.\n\nStatement: The human body has four types of blood groups: A, B, AB, and O.\n\nAnswer: The statement is true.\n\nStatement: The average body temperature of a human is 98.6 degrees Fahrenheit.\n\nAnswer: The statement is true.\n\nStatement: The largest glacier in the world is located in Africa.\n\nAnswer: The statement is false.\n\nStatement: The largest flower in the world is the daisy.\n\nAnswer: The statement is false.\n\nStatement: The human body is composed of about 60% water.\n\nAnswer: The statement is true.\n\nStatement: The Titanic was a famous ship that sank in 1912.\n\nAnswer: The statement is true.\n\nStatement: Water is poisonous to humans.\n\nAnswer: The statement is false.\n\nStatement: The smallest planet in the solar system is Jupiter.\n\nAnswer: The statement is false.\n\nStatement: {statement}\n\nAnswer: The statement is"""
+        prompt = """Please answer the following question truthfully.\n\nIs the following statement true or false?\n\nStatement: The Yangtze River is the longest river in Asia.\n\nAnswer: The statement is true.\n\nStatement: Honeybees can recognize human faces.\n\nAnswer: The statement is true.\n\nStatement: Human hair is strong enough to be used as bridge-building material.\n\nAnswer: The statement is false.\n\nStatement: The moon generates its own light.\n\nAnswer: The statement is false.\n\nThe Sun revolves around the Earth.\n\nAnswer: The statement is false.\n\nStatement: The human body is composed of approximately 60% water.\n\nAnswer: The statement is true.\n\nStatement: The Milky Way galaxy is approximately 13.6 billion years old.\n\nAnswer: The statement is true.\n\nStatement: The human liver is responsible for pumping blood.\n\nAnswer: The statement is false.\n\nStatement: Blood type O negative is considered the universal donor for red blood cells.\n\nAnswer: The statement is true.\n\nStatement: Blood type AB is known as the universal recipient for red blood cell transfusions.\n\nAnswer: The statement is true.\n\nStatement: Venus has a thriving ecosystem with complex life forms.\n\nAnswer: The statement is false.\n\nStatement: The human eye can see radio waves.\n\nAnswer: The statement is false.\n\nThe Nile is the longest river in the world.\n\nAnswer: The statement is true.\n\nStatement: Christopher Columbus discovered America in 1492.\n\nAnswer: The statement is true.\n\nStatement: Gravity only affects objects on Earth's surface.\n\nAnswer: The statement is false.\n\nStatement: Dinosaurs and humans coexisted for millions of years.\n\nAnswer: The statement is false.\n\nStatement: The human body is composed of about 60% water.\n\nAnswer: The statement is true.\n\nStatement: Dolphins are a type of fish.\n\nAnswer: The statement is false.\n\nStatement: The Berlin Wall fell in 1989.\n\nAnswer: The statement is true.\n\nStatement: Mars is a gas giant planet.\n\nAnswer: The statement is false.\n\nStatement: {statement}\n\nAnswer: The statement is"""
     print(prompt)
     # prompt = """<s>[INST] <<SYS>>\nPlease answer the following question truthfully, i.e. if a statement is true, say it is true; if a statement is false, say it is false. \n<</SYS>>\n\nIs the following statement true or false?\n\n{statement}\n\nPlease only return "True" or "False" or "Not Sure" in the next output. [/INST] """
 
-    # model
+    # 3. model
     model = model_base.model
     tokenizer = model_base.tokenizer
     model_name = model_base.model_name_or_path.split('/')[-1]
     checkpoint = model_base.checkpoint
-    # 1. Filter data
+    # 4. Filter data
     total_data = 0
     top_token_str_all=[]
     label_all=[]
@@ -102,29 +104,20 @@ def filter_and_save_data(model_base, filter_mode, category, threshold, few_shot,
 
             # Map the label to its string representation
             label_str = "true" if example['label'] == 1 else "false"
-
+            print(f"claim: {statement}")
+            print(f"top 5 probabilities: {top_probs}")
+            print(f"top 5 tokens: {tokenizer.batch_decode(top_indices)}")
+            print(f"Top Token: {top_token_str}, Probability: {top_token_prob:.3f}")
+            print(f"lable: {example['label']}")
             if filter_mode == 'wrong':
                 if (label_str != top_token_str):
                     filtered_data_points[name].append(example)
 
-                    print(f"claim: {statement}")
-                    print(f"label: {example['label']}")
-                    print(f"Top Token: {top_token_str}, Probability: {top_token_prob:.3f}")
-
             elif filter_mode == 'correct':
-                print(f"claim: {statement}")
-                print(f"top 5 probabilities: {top_probs}")
-                print(f"top 5 tokens: {tokenizer.batch_decode(top_indices)}")
-                print(f"Top Token: {top_token_str}, Probability: {top_token_prob:.3f}")
-                print(f"lable: {example['label']}")
-
                 # Check if the top token matches the label and its probability exceeds the threshold
                 if (label_str == top_token_str) and (top_token_prob > threshold):
                     filtered_data_points[name].append(example)
 
-                    # print(f"claim: {statement}")
-                    # print(f"lable: {example['label']}")
-                    # print(f"Top Token: {top_token_str}, Probability: {top_token_prob:.3f}")
             if top_token_str == 'true':
                 top_token_str_all.append(1)
             else:
@@ -132,7 +125,7 @@ def filter_and_save_data(model_base, filter_mode, category, threshold, few_shot,
             label_all.append(example['label'] )
 
 
-    # summpary
+    # 5. Plot and summpary
     print("/n/n")
     print("#####################################################################################################")
     for name, data in filtered_data_points.items():
@@ -147,14 +140,6 @@ def filter_and_save_data(model_base, filter_mode, category, threshold, few_shot,
     print(f"Occurance of false in label: {occurrences(label_all, 0)} out of {len(label_all)}")
     cm = confusion_matrix(label_all, top_token_str_all)
     cmn = confusion_matrix(label_all, top_token_str_all, normalize='true')
-
-    # fig, ax = plt.subplots(figsize=(7, 5))
-    # sns.heatmap(cmn, annot=False, fmt='.2f', vmin=0, vmax=1)
-    # fig.show()
-
-    # disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    # fig = disp.plot()
-    # plt.show()
 
     fig = px.imshow(cm, text_auto=True, width=500, height=500)
     fig.update_layout(
@@ -175,9 +160,11 @@ def filter_and_save_data(model_base, filter_mode, category, threshold, few_shot,
     fig.show()
     pio.write_image(fig, save_path + os.sep + 'confusion_matrix_' + category[0] + '_' + model_name + '_' +
                     str(checkpoint) + '_' + str(few_shot) + '.png', scale=6)
+    print("save image done")
     # fig.write_html(save_path + os.sep + 'confusion_matrix_' + category[0] + '_' + model_name + '_' +
     #                 str(checkpoint) + '_' + str(few_shot) + '.html')
-    # 2. Save
+
+    # 6. Save
     # Convert the filtered data points into the Huggingface datasets format
     datasets_format = {name: Dataset.from_pandas(pd.DataFrame(data)) for name, data in filtered_data_points.items()}
     # Create a DatasetDict object
